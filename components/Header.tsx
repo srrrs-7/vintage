c981b4624c77713757d1c0ff0bacfbfb4dbe643a
sRRRs <srrrs4510@gmail.com>
2023-02-20 12:13:42 +0900

    vintage site fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      <div className='flex z-50 justify-between items-center py-3 px-40 w-full'>
+      <div className='flex z-50 justify-between items-center py-3 px-40 w-full sm:justify-center sm:items-center'>
         <Link href='/'>
           <Image
             src='/logo.png'
@@ -15,9 +15,10 @@ const Header = () => {
             className='bg-blue-800 rounded-full cursor-pointer'
           />
         </Link>
-        <div className='flex justify-center items-center p-1 space-x-20'>
+
+        <div className='flex justify-center items-center p-1 space-x-20 sm:space-x-2'>
           <Link href='/'>
-            <a className='block py-2 pr-2 mx-4 font-serif text-xl text-white hover:text-orange-400 transition duration-150 ease-in-out hover:scale-110 lg:px-2'>
+            <a className='block py-2 pr-1 mx-4 font-serif text-xl text-white hover:text-orange-400 transition duration-150 ease-in-out hover:scale-110 lg:px-2'>
               Home
             </a>
           </Link>
--- components/MovieSlider.tsx
+++ components/MovieSlider.tsx
@@ -4,21 +4,15 @@ const MovieSlider = () => {
   return (
     <>
       <div>
-        <video
-          className='flex fixed top-0 left-0 z-10 mt-20 min-w-full'
-          playsInline
-          autoPlay
-          muted
-          loop
-        >
+        <video className='flex fixed left-0 z-10 w-[100%]' playsInline autoPlay muted loop>
           <source src='vintage.mp4' type='video/mp4' />
         </video>
 
         <div className='z-20'>
-          <p className='flex fixed z-10 justify-center w-[100%] h-[100%] font-serif text-9xl text-gray-300 underline underline-offset-4 animate-pulse sm:top-48 sm:text-6xl md:top-64 lg:top-96'>
+          <p className='flex fixed z-10 justify-center w-[100%] h-[100%] font-serif text-gray-300 underline underline-offset-4 animate-pulse sm:top-32 sm:text-xl md:top-64 lg:top-96'>
             V I N T A G E
           </p>
-          <p className='flex fixed z-10 justify-center mt-28 w-[100%]  h-[100%] font-serif text-6xl text-gray-200 animate-pulse sm:top-48 sm:text-3xl md:top-64 lg:top-96'>
+          <p className='flex fixed z-10 justify-center mt-28 w-[100%]  h-[100%] font-serif text-gray-200 animate-pulse sm:top-12 md:top-44 lg:top-80 sm:text-md'>
             Hair Studio
           </p>
         </div>
